b34f9b3d913728e2c850638d2cce318982375455
Shary Chuc <sharychuc@gmail.com>
2015-11-04 18:27:58 -0500

    UbicacionAux

---
+++ app/src/main/java/rest/UbicacionAux.java
@@ -0,0 +1,79 @@
+package rest;
+
+/**
+ * Created by Shary on 18/10/2015.
+ */
+public class UbicacionAux {
+    private String Pais;
+    private String Estado;
+    private String Ciudad;
+    private String Direccion;
+    private String Latitud;
+    private String Longitud;
+    private String Fecha;
+    private int PacienteId;
+
+    public String getPais() {
+        return Pais;
+    }
+
+    public void setPais(String pais) {
+        Pais = pais;
+    }
+
+    public String getEstado() {
+        return Estado;
+    }
+
+    public void setEstado(String estado) {
+        Estado = estado;
+    }
+
+    public String getCiudad() {
+        return Ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        Ciudad = ciudad;
+    }
+
+    public String getDireccion() {
+        return Direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        Direccion = direccion;
+    }
+
+    public String getLatitud() {
+        return Latitud;
+    }
+
+    public void setLatitud(String latitud) {
+        Latitud = latitud;
+    }
+
+    public String getLongitud() {
+        return Longitud;
+    }
+
+    public void setLongitud(String longitud) {
+        Longitud = longitud;
+    }
+
+    public String getFecha() {
+        return Fecha;
+    }
+
+    public void setFecha(String fecha) {
+        Fecha = fecha;
+    }
+
+    public int getPacienteId() {
+        return PacienteId;
+    }
+
+    public void setPacienteId(int pacienteId) {
+        PacienteId = pacienteId;
+    }
+}
